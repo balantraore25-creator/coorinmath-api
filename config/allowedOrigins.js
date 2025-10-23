159e51e6986bebccf4e451f13071bc6257856f97
BALAN <balantraore@hotmail.com>
2025-10-23 22:10:57 +0200

    deployment

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
-  'https://coorinmath.onrender.com/api'
+  'https://coorinmath.onrender.com',   // ton frontend Render
+  'http://localhost:5173',             // ton frontend local (Vite)
 ]
 
 module.exports = allowedOrigins
-
--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,15 +1,16 @@
 const allowedOrigins = require('./allowedOrigins')
 
 const corsOptions = {
-    origin: (origin, callback) => {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'))
-        }
-    },
-    credentials: true,
-    optionsSuccessStatus: 200
+  origin: (origin, callback) => {
+    // Autoriser si l'origine est dans la liste ou si la requête n'a pas d'origine (ex: Postman, cURL)
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error(`Not allowed by CORS: ${origin}`))
+    }
+  },
+  credentials: true,            // Autorise l'envoi de cookies/headers d'auth
+  optionsSuccessStatus: 200     // Pour compatibilité avec certains navigateurs (IE, SmartTVs)
 }
 
-module.exports = corsOptions 
+module.exports = corsOptions
